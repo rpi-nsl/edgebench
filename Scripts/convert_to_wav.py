276bd694a6e173610777ec0d505d258db76633f0
akaanirban <anirban.dash@gmail.com>
2018-11-28 11:46:02 -0500

    Modified the audio conversion script to take in arguments

--- Scripts/convert_to_wav.py
+++ Scripts/convert_to_wav.py
@@ -1,10 +1,18 @@
 import subprocess
 import os
+import argparse
 
-# Convert audio files to specific wav format for pocketshpinx
+"""
+Convert audio files to specific wav format for pocketshpinx
+According to : https://cmusphinx.github.io/wiki/faq/
+"""
+parser = argparse.ArgumentParser()
+parser.add_argument('-s', "--source", help="Source Directory", required=True)
+parser.add_argument('-d', "--destination", help="Destination Directory", required=True)
+args = parser.parse_args()
 
-DIRNAME = "<input directory of audio files>"
-OUTPUTDIR = "<output directory of audio files>"
+Sourcdir = args.source
+Destinationdir = args.destination
 
 
 def get_file_paths(dirname):
@@ -17,10 +25,11 @@ def get_file_paths(dirname):
 
 
 if __name__ == "__main__":
-	all_files = get_file_paths(DIRNAME)
-	response = subprocess.run(['mkdir', OUTPUTDIR], 
+	all_files = get_file_paths(Sourcdir)
+	response = subprocess.run(['mkdir', Destinationdir], 
 	                          shell = False, stderr=subprocess.PIPE)
+	print("Converting all audio files to 16khz 16bit little-endian mono wav files")
 	for file in all_files:
 		new_filename = file.split(os.sep)[-1].split('.')[0]+ ".wav"
-		response = subprocess.run(['ffmpeg', '-i', file, '-ar', '16000', '-ac', '1', os.path.join(OUTPUTDIR, new_filename)], 
+		response = subprocess.run(['ffmpeg', '-i', file, '-ar', '16000', '-ac', '1', os.path.join(Destinationdir, new_filename)], 
 	                          shell = False, stderr=subprocess.PIPE)
